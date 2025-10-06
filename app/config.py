5dccdc000694ec493aaa43b6139d25737df541d1
moynul2k3 <moynul.officials@gmail.com>
2025-10-06 17:11:13 +0600

    1st commit

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ class Settings(BaseSettings):
     DEBUG: bool = True
     ENV: str = "development"
     
-    DATABASE_URL: str = "mysql://root:root@localhost:3306/mydb"
+    # DATABASE_URL: str = "mysql://root:root@localhost:3306/mydb"
     # DATABASE_URL: str = "postgres://postgres:root@localhost:5432/mydb"
-    # DATABASE_URL: str = "postgres://neondb_owner:npg_kY0Qo2RUXZVL@ep-damp-scene-adfwy4vs-pooler.c-2.us-east-1.aws.neon.tech/neondb"
+    DATABASE_URL: str = "postgres://neondb_owner:npg_kY0Qo2RUXZVL@ep-damp-scene-adfwy4vs-pooler.c-2.us-east-1.aws.neon.tech/neondb"
     SECRET_KEY: Optional[str] = "c6dcf58058a6ce5204199818a25eed7eb58b6758a20df0385e29dbea6b49873dccad7449f8022dc193da4616ba10c97457aa2e16e0b2c5b0e5555fe1ac492aa1"
 
     class Config:
